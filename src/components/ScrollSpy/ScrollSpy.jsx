42832aad45d78ecbc20cfdd33ff65f59e38e9720
Ready to Die <54882064+Abhid1239@users.noreply.github.com>
2023-09-23 18:13:45 +0530

    scrollSPyWorking

--- src/components/ScrollSpy/ScrollSpy.jsx
+++ src/components/ScrollSpy/ScrollSpy.jsx
@@ -9,58 +9,50 @@ function ScrollSpy({ headContent, childContent, shouldSticky = false }) {
 	const activeIndexRef = useRef(0);
 
 	const onHeadClick = (id) => {
-		let childProps = childrenRef.current && childrenRef.current.children[id].getBoundingClientRect();
-		window.scrollBy({ top: childProps.top - parentRef.current.offsetHeight, behavior: 'smooth' });
+		const activeBoundingRect = childrenRef?.current?.children[id]?.getBoundingClientRect();
+		const scrollToPosition = activeBoundingRect.top - parentRef.current.offsetHeight;
+		window.scrollBy({ top: scrollToPosition, behavior: 'smooth' });
 	};
 	const changeSelector = () => {
-		let childProps = childrenRef.current && childrenRef.current.children[activeIndexRef.current].getBoundingClientRect();
-		if (childProps.height + childProps.top - parentRef.current.offsetHeight - 100 < 0 && activeIndexRef.current < childrenRef.current.children.length - 1) {
+		const activeBoundingRect = childrenRef?.current?.children[activeIndexRef.current]?.getBoundingClientRect();
+		const topHeadOffset = parentRef?.current?.offsetHeight + 100; // skeptical
+		const isNotFirstChild = activeIndexRef.current > 0;
+		const isNotLastChild = activeIndexRef.current < childrenRef.current.children.length - 1;
+		if (activeBoundingRect.height + activeBoundingRect.top < topHeadOffset && isNotLastChild) {
 			setActiveIndex(activeIndexRef.current + 1);
 			activeIndexRef.current = activeIndexRef.current + 1;
-		} else if (childProps.top > parentRef.current.offsetHeight + 100 && activeIndexRef.current !== 0) {
+			console.log('a');
+		} else if (activeBoundingRect.top > topHeadOffset && isNotFirstChild) {
 			setActiveIndex(activeIndexRef.current - 1);
 			activeIndexRef.current = activeIndexRef.current - 1;
+			console.log('b');
 		}
-		// console.log(childProps.height + childProps.top - parentRef.current.offsetHeight - 100, activeIndexRef.current);
-		// console.log(childProps.height, childProps.top, parentRef.current.offsetHeight, 100);
 	};
 	useEffect(() => {
-		document.addEventListener('scroll', () => changeSelector());
-		// let observer = new IntersectionObserver(
-		// 	(enteries) => {
-		// 		enteries.forEach((entry) => {
-		// 			let currentIndex = parseInt(entry.target.id);
-		// 			console.log(currentIndex, entry.isIntersecting, entry.isVisible, entry);
-
-		// 			if (entry.isIntersecting) {
-		// 				if (currentIndex != activeIndexRef.current.active) {
-		// 					setActiveIndex(currentIndex);
-		// 					activeIndexRef.current.prev = activeIndexRef.current.active;
-		// 					activeIndexRef.current.active = currentIndex;
-		// 					// console.log(activeIndexRef.current, currentIndex);
-		// 				}
-		// 			} else {
-		// 				if (activeIndexRef.current.active == currentIndex) {
-		// 					setActiveIndex(currentIndex++);
-		// 					activeIndexRef.current.active = currentIndex;
-		// 				}
-		// 			}
-		// 		});
-		// 	},
-		// 	{
-		// 		threshold: 0
-		// 		rootMargin: '-200px',
-		// 	}
-		// );
-		// Array.from(childrenRef.current.children).forEach((ele, ind) => observer.observe(ele));
+		window.addEventListener('scroll', changeSelector);
+		return () => {
+			window.removeEventListener('scroll', changeSelector);
+		};
 	}, []);
 
 	const scrollSpyHead = (Sticky = false) => {
 		return (
-			<nav ref={parentRef} className={`${styles.headWrapper} ${Sticky ? styles.stickyHeaderStyles : ''}`}>
+			<nav 
+				ref={parentRef}
+				className={`
+					${styles.headWrapper} 
+					${Sticky ? styles.stickyHeaderStyles : ''}
+				`}
+			>
 				{headContent &&
 					headContent.map((cont, id) => (
-						<div key={cont} onClick={() => onHeadClick(id)} className={`${styles.headData} ${id === activeIndex ? styles.activeheadData : ''}`}>
+						<div 
+							key={cont} 
+							onClick={() => onHeadClick(id)} 
+							className={`
+								${styles.headData} ${id === activeIndex ? styles.activeheadData : ''}
+							`}
+						>
 							{cont}
 						</div>
 					))}
@@ -80,153 +72,9 @@ function ScrollSpy({ headContent, childContent, shouldSticky = false }) {
 	};
 	return (
 		<div className={styles.scrollSpy}>
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>{' '}
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>{' '}
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>{' '}
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>{' '}
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>{' '}
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>{' '}
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>
 			{shouldSticky && scrollSpyHead(true)}
 			{scrollSpyHead()}
 			{srollSpyContent()}
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>{' '}
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>{' '}
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>{' '}
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>{' '}
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>{' '}
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>{' '}
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>{' '}
-			<p>
-				Id nisi est do proident reprehenderit aliquip. Nisi nostrud labore nostrud ad nostrud exercitation reprehenderit minim. Sit exercitation ex ad reprehenderit laboris. Dolore
-				reprehenderit laboris adipisicing cupidatat adipisicing et cillum sint reprehenderit adipisicing ex. Officia velit excepteur ea sit mollit esse. Pariatur sint ipsum in ea ex cillum
-				officia sit cillum et magna incididunt do. Sint esse voluptate proident culpa sit sint reprehenderit mollit velit pariatur enim est mollit. Labore nulla et nisi occaecat consequat
-				aliquip do in tempor sint laboris et quis.
-			</p>
 		</div>
 	);
 }
